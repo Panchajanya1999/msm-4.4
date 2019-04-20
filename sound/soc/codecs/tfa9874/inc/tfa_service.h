030116e789c13bfc9b92a594645846b22ec183cd
DarknessKiller <kaelvinchen2@gmail.com>
2019-04-20 08:59:06 +0800

    Update Audio CAF
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- sound/soc/codecs/tfa9874/inc/tfa_service.h
+++ sound/soc/codecs/tfa9874/inc/tfa_service.h
@@ -20,7 +20,7 @@ extern "C" {
 #include "NXP_I2C.h"
 #endif
 
-#define TFA98XX_API_REV_STR "v6.5.5"
+#define TFA98XX_API_REV_STR "v6.6.2"
 
 #include "tfa_device.h"
 
@@ -997,6 +997,7 @@ enum Tfa98xx_Error tfa_status(struct tfa_device *tfa);
  */
 int tfa_dev_get_mtpb(struct tfa_device *tfa);
 
+enum Tfa98xx_Error tfaGetFwApiVersion(struct tfa_device *tfa, unsigned char *pFirmwareVersion);
 #ifdef __cplusplus
 }
 #endif
